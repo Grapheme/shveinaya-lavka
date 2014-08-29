71350ff5769ccdf5aeeb8162ec80040999b28ea1
KostyaD <thedamaxstudio@gmail.com>
2014-08-29 19:02:15 +0400

    something for last commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,9 @@
+var mobile_menu = (function(){
+	var init = function() {
+		$(document).on('click touchend', '.menu-icon', function(){
+			$('.mobile-header').toggleClass('open');
+		});
+	}
 
+	init();
+})();
