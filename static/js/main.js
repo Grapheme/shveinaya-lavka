2b317f80ebe46d4a2eaab20c609ab077505e195e
KostyaD <thedamaxstudio@gmail.com>
2014-08-29 19:05:48 +0400

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 var mobile_menu = (function(){
 	var init = function() {
-		$(document).on('click touchend', '.menu-icon', function(){
+		$(document).on('click', '.menu-icon', function(){
 			$('.mobile-header').toggleClass('open');
 		});
 	}
